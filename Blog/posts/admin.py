6e0ef31732fd1175a0d52e1eccdd5518ad4ce57a
Izaim <izaim99@gmail.com>
2016-01-11 21:55:53 +0100

    Added Post 'Created', 'Updated' timestamp also Search by 'Title' and 'Content'. Filter by 'Created' and 'Updated' time.

--- Blog/posts/admin.py
+++ Blog/posts/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "created", "updated"]
+    list_display_links = ["title"]
+    list_filter = ["created", "updated"]
+    #list_editable = ["title"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Simple-Blog-Django
-#### A quickstart for those who want to build a Blog using Python/Django.
+#### A quickstart for those who want to build a Blog using Python/Django 1.9.11 (virtualenv).
+
+- Users/User Groups
+- CRUD Posts
+- Search Posts
+- Messages
+- Pagination
+- Social Share Links
+- Bootstrap - Views

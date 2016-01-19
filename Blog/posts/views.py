709d33ef176c4a5eccb2e7e8c55f21bdb6a3abf3
Izaim <izaim99@gmail.com>
2016-01-20 00:28:59 +0100

    redesigned urls and updated dependencies

--- Blog/posts/views.py
+++ Blog/posts/views.py
@@ -1,15 +1,28 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 # Create your views here.
 def post_create(request): #create post
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request): #read post
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+        "title": "Detail"
+    }
+    return render(request,"index.html", context)
+    #return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request): #list posts
-    return HttpResponse("<h1>List</h1>")
+    if request.user.is_authenticated():
+        context ={
+            "title": "List"
+        }
+    else:
+        context ={
+            "title": "List"
+        }
+    return render(request,"index.html", context)
+    #return HttpResponse("<h1>List</h1>")
 
 def post_update(request): #update post
     return HttpResponse("<h1>Update</h1>")

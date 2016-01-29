ffa91aa69a5fa3a51ef3f1b356599ff927ce3616
Izaim <izaim99@gmail.com>
2016-01-29 01:26:33 +0100

    Listing items on Index using queryset

--- Blog/posts/views.py
+++ Blog/posts/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def post_create(request): #create post
     return HttpResponse("<h1>Create</h1>")
@@ -13,14 +15,19 @@ def post_detail(request): #read post
     #return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request): #list posts
-    if request.user.is_authenticated():
-        context ={
-            "title": "List"
-        }
-    else:
-        context ={
-            "title": "List"
-        }
+    queryset = Post.objects.all()
+    context ={
+        "object_list": queryset,
+        "title": "My User List"
+    }
+    # if request.user.is_authenticated():
+    #     context ={
+    #         "title": "My User List"
+    #     }
+    # else:
+    #     context ={
+    #         "title": "List"
+    #     }
     return render(request,"index.html", context)
     #return HttpResponse("<h1>List</h1>")
 

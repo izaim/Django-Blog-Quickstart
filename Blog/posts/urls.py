661dd33c7e569f4c8420f61bc32bb6bd0328ab80
Izaim <izaim99@gmail.com>
2016-02-10 23:18:00 +0100

    action messages, post delete

--- Blog/posts/urls.py
+++ Blog/posts/urls.py
@@ -18,9 +18,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list), #main page
+    url(r'^$', views.post_list, name="list"), #main page
     url(r'^create/$', views.post_create),
     url(r'^(?P<id>\d+)/$', views.post_detail, name="detail"),
     url(r'^(?P<id>\d+)/edit/$', views.post_update, name="update"),
-    url(r'^delete/$', views.post_delete),
+    url(r'^(?P<id>\d+)/delete/$', views.post_delete),
 ]
--- Blog/posts/views.py
+++ Blog/posts/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
 
@@ -11,13 +12,14 @@ def post_create(request): #create post
         instance = form.save(commit=False)
         print form.cleaned_data.get("title")
         instance.save()
-        print "Post Created Successfully."
+        messages.success(request,"Article Posted Successfully!", extra_tags="glyphicon glyphicon-ok")
         return HttpResponseRedirect(instance.get_absolute_url())
+
     context = {
         "form": form,
     }
-
     return render(request,"form.html", context)
+
 
 def post_detail(request, id): #read post
     instance = get_object_or_404(Post,id=id)
@@ -27,6 +29,7 @@ def post_detail(request, id): #read post
     }
     return render(request,"single_post.html", context)
 
+
 def post_list(request): #list posts
     queryset = Post.objects.all()
     context ={
@@ -35,15 +38,16 @@ def post_list(request): #list posts
     }
     return render(request,"index.html", context)
 
+
 def post_update(request, id=None): #update post
     instance = get_object_or_404(Post,id=id)
     form = PostForm(request.POST or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
-        print form.cleaned_data.get("title")
         instance.save()
-        print "Post Edited Successfully."
+        messages.success(request,"Article Edited Successfully!", extra_tags="glyphicon glyphicon-ok")
         return HttpResponseRedirect(instance.get_absolute_url())
+
     context = {
         "title": instance.title,
         "instance": instance,
@@ -51,5 +55,9 @@ def post_update(request, id=None): #update post
     }
     return render(request,"form.html", context)
 
-def post_delete(request): #delete post
-    return HttpResponse("<h1>Delete</h1>")
+
+def post_delete(request, id=None): #delete post
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request,"Article Deleted Successfully!", extra_tags="glyphicon glyphicon-ok")
+    return redirect("posts:list")

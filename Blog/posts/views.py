e420183c3586e71210ef0b1a5add7ee6e6f27aff
Izaim <izaim99@gmail.com>
2016-02-16 01:14:49 -0800

    Pagination, 5 posts per page

--- Blog/posts/views.py
+++ Blog/posts/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 
 # Create your views here.
 def post_create(request): #create post
@@ -31,11 +34,25 @@ def post_detail(request, id): #read post
 
 
 def post_list(request): #list posts
-    queryset = Post.objects.all().order_by("-created")
+    queryset_list = Post.objects.all().order_by("-created")
+    paginator = Paginator(queryset_list, 5) #Show 25 contacts per page
+
+    #pagination
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context ={
         "object_list": queryset,
         "title": "My Posts"
     }
+
     return render(request,"posts.html", context)
 
 
